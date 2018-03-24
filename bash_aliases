80d7f41a3fe5bb1327569733779f7f561c33d582
paulourriza <pmurriza@gmail.com>
2018-03-24 13:20:01 -0700

    automate symlinking with makelinks.sh, add bash dotfiles

---
+++ bash_aliases
@@ -0,0 +1,2 @@
+alias clrdocker='docker rm $(docker ps -a -q -f status=exited)'
+alias tmux='tmux -u'
---
+++ makelinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run this script to generate all necessary symlinks to dotfiles in *nix
+# author: Paulo Urriza
+# email: pmurriza@gmail.com
+
+ln -sf ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
