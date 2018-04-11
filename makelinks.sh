23e6509b9534671df9261c92083f64ef1d6411af
paulourriza <pmurriza@gmail.com>
2018-04-11 11:02:33 -0700

    added gitconfig to dotfiles and created entry in makelinks.sh

--- makelinks.sh
+++ makelinks.sh
@@ -8,3 +8,4 @@ ln -sf ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
