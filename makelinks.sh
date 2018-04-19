566974074093aecd223897ec26950b367cfac6d3
paulourriza <pmurriza@gmail.com>
2018-04-18 21:35:56 -0700

    link zshrc, autocrlf=false

--- makelinks.sh
+++ makelinks.sh
@@ -9,3 +9,4 @@ ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/zshrc ~/.zshrc
